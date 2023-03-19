6a119b4cc439d6a9c5cc537206e0e077e06eaf62
Yu10cheng <aa8180505@hotmail.com>
2023-03-19 16:23:20 +1100

    update

---
+++ Assets/Code/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // reference to the Rigidbody component called "Rb"
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // We marked this as "Fixed" Update becasue we
+    //are using it to mess with physics.
+    void FixedUpdate()
+    {
+        //add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
