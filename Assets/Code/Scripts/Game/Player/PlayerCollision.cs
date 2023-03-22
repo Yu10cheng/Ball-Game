f67e6101488a33e06f18ceb0cc751ff3a0471d75
Yu Cheng <aa8180505@hotmail.com>
2023-03-22 17:25:44 +1100

    update
    Fit more different patch of unity

--- Assets/Code/Scripts/Game/Player/PlayerCollision.cs
+++ Assets/Code/Scripts/Game/Player/PlayerCollision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
-    
+    [SerializeField] private GameManager _gameManager;
 
     
     private void OnCollisionEnter(Collision collisionInfo)
@@ -14,7 +14,7 @@ public class PlayerCollision : MonoBehaviour
         if (collisionInfo.collider.tag == "Obstacle")
         {
             GetComponent<PlayerMovement>().enabled = false;
-            FindAnyObjectByType<GameManager>().EndGame();
+            _gameManager.EndGame();
 
         }
     }
